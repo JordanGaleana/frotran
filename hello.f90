2fd9b66748d87a38e8a30b68c387117dda214c78
Jordan <gabodeth97@gmail.com>
2022-07-20 13:34:38 -0500

    Agregando nombre a hello y mas numeros a suma

--- hello.f90
+++ hello.f90
@@ -1,3 +1,6 @@
 program hello
-        write *, "Hello World"
+       character(20):myname
+       write *, "Ingrese nombre"
+       read*, myname
+       write *, "Hello World", myname
 end program hello
